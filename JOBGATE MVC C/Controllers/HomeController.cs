c4931b0e5529963eee69d0b94adfad3ef6e43f9f
lovelive-cat <77373829+lovelive-cat@users.noreply.github.com>
2021-02-05 08:59:37 +0700

    Test Upload README.md

---
+++ JOBGATE MVC C/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using JOBGATE_MVC_C.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JOBGATE_MVC_C.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Home()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        public IActionResult Register_EPY()
+        {
+            return View();
+        }
+
+        public IActionResult Register_CPN()
+        {
+            return View();
+        }
+
+        public IActionResult Register_Result()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
