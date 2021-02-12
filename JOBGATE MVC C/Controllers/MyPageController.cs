f51447c2746d2750be5f8e85fcaa7a49de378cb0
S1XS3N3S <beam.jobgate@gmail.com>
2021-02-12 15:15:29 +0700

    Resume Page Add

---
+++ JOBGATE MVC C/Controllers/MyPageController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JOBGATE_MVC_C.Controllers
+{
+    public class MyPageController : Controller
+    {
+        public IActionResult MyPage()
+        {
+            return View();
+        }
+
+        public IActionResult Resume()
+        {
+            return View();
+        }
+    }
+}
